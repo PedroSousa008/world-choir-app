b0269da9c7cd47132573a2531867cafe61080eae
Pedro Castro de Sousa <pedrocastrosousa@Mac.Home>
2026-08-28 15:30:07 +0100

    Force refresh transparent locked stamp and remove frame background

--- public/js/world-choir-config.js
+++ public/js/world-choir-config.js
@@ -117,7 +117,7 @@ const WorldChoirConfig = (() => {
    */
   const PASSPORT_STAMP_WORLD_CHOIR_2027_I_SANG_LOCKED = {
     src: 'images/passport/stamps/world-choir-2027-i-sang-locked.png',
-    version: '20260828b',
+    version: '20260828d',
     width: 1305,
     height: 1206,
     alt: 'Locked World Choir 2027 stamp',
