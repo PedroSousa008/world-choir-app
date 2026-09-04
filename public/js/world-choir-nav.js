a1c80bd656077b513bc87ba3b17d1176c6b8aee2
Pedro Castro de Sousa <pedrocastrosousa@Mac.Home>
2026-09-04 13:20:43 +0100

    Fix nav prefetch for foundation cards Total Raised update

--- public/js/world-choir-nav.js
+++ public/js/world-choir-nav.js
@@ -61,7 +61,7 @@ const WorldChoirNav = (() => {
       'css/donate.css?v=20260904bu',
       'js/donate/creator-foundations-store.js?v=20260904bu',
       'js/donate/donation-flow.js?v=20260831a',
-      'js/foundation-public-card.js?v=20260904cb
+      'js/foundation-public-card.js?v=20260904cb',
       'js/donate/donate-page.js?v=20260904bu',
       '/api/creator-foundations',
       '/api/donations?action=config',
