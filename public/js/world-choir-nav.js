88334040e2ecfd3f9de6a4a976bdea45eec52167
Pedro Castro de Sousa <pedrocastrosousa@unknownceae1aefbe2a.lan>
2026-09-07 15:56:03 +0100

    Align Home TAB_ASSETS cache-bust with People Sang freeze

--- public/js/world-choir-nav.js
+++ public/js/world-choir-nav.js
@@ -422,7 +422,7 @@ const WorldChoirNav = (() => {
     home: [
       'index.html',
       'css/home.css?v=20260905b',
-      'js/world-choir-home.js?v=20260906black',
+      'js/world-choir-home.js?v=20260907peoplesang',
       'js/world-choir-db.js?v=20260906perf2',
     ],
     map: [
