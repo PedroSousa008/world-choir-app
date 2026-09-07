dc48c783a7c5f0016ddc02a288fe9a1a8f27ce06
Pedro Castro de Sousa <pedrocastrosousa@unknownceae1aefbe2a.lan>
2026-09-07 16:14:42 +0100

    Align Map TAB_ASSETS cache-bust with post-event join

--- public/js/world-choir-nav.js
+++ public/js/world-choir-nav.js
@@ -433,7 +433,7 @@ const WorldChoirNav = (() => {
       'js/map/sponsor-bar.js?v=20260905a',
       '/api/map-sponsors',
       'js/world-choir-map-tiles.js?v=20260906mapfix2',
-      'js/world-choir-map.js?v=20260906black',
+      'js/world-choir-map.js?v=20260907a',
       'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
       'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
       'https://unpkg.com/maplibre-gl@4.7.1/dist/maplibre-gl.css',
