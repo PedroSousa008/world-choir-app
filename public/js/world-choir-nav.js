d6b0faba277fc02188249230ea31b7649ae7571d
Pedro Castro de Sousa <pedrocastrosousa@Mac.Home>
2026-09-11 17:55:38 +0100

    Light Mode: Daily Acts mystery ? icons white

--- public/js/world-choir-nav.js
+++ public/js/world-choir-nav.js
@@ -626,7 +626,7 @@ const WorldChoirNav = (() => {
     ],
     'daily-acts': [
       'daily-acts.html',
-      'css/daily-acts-page.css?v=20260911dapQ2',
+      'css/daily-acts-page.css?v=20260911dapWhiteQ',
       'js/daily-acts-page.js?v=20260906black',
     ],
   };
