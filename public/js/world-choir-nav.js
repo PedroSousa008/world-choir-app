4299e89f3f8211f17fa0bc83a0bb3a8beef06237
Pedro Castro de Sousa <pedrocastrosousa@Mac.Home>
2026-09-11 17:52:56 +0100

    Cache-bust Daily Acts CSS so Light Mode ? icon update loads

--- public/js/world-choir-nav.js
+++ public/js/world-choir-nav.js
@@ -626,7 +626,7 @@ const WorldChoirNav = (() => {
     ],
     'daily-acts': [
       'daily-acts.html',
-      'css/daily-acts-page.css?v=20260911dapQ',
+      'css/daily-acts-page.css?v=20260911dapQ2',
       'js/daily-acts-page.js?v=20260906black',
     ],
   };
