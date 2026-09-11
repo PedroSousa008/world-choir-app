4c5eb47ef13d14adf39f7e953b13af8d86ca158a
Pedro Castro de Sousa <pedrocastrosousa@Mac.Home>
2026-09-11 18:02:38 +0100

    Light Mode: Daily Act detail panel clear grey

--- public/js/world-choir-nav.js
+++ public/js/world-choir-nav.js
@@ -626,7 +626,7 @@ const WorldChoirNav = (() => {
     ],
     'daily-acts': [
       'daily-acts.html',
-      'css/daily-acts-page.css?v=20260911calMonth',
+      'css/daily-acts-page.css?v=20260911sheetGrey',
       'js/daily-acts-page.js?v=20260906black',
     ],
   };
